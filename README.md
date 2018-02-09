3beb60486ea75f7eac3f329f48091ef26a8d7be1
Sandeep Jadoonanan <jsanweb@gmail.com>
2018-02-09 08:43:46 -0500

    Add setuptools config. Modify README.md

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ get-secret
 
 [![Build Status](https://travis-ci.org/TunedMystic/get-secret.svg?branch=master)](https://travis-ci.org/TunedMystic/get-secret)
 
-Simple function to fetch env vars or secrets for your application.
+Simple tool to fetch secrets for your application.
---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='get-secret',
+    version='0.0.1',
+    py_modules=['get_secret'],
+    test_suite='tests',
+    description='Simple tool to fetch secrets for your application.',
+    long_description=readme(),
+    author='Sandeep Jadoonanan',
+    author_email='jsanweb@gmail.com',
+    url='https://github.com/tunedmystic/get-secret',
+    download_url=(
+        'https://github.com/tunedmystic/get-secret/archive/0.0.1.tar.gz'
+    ),
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords=['secret', 'env', 'docker'],
+)
